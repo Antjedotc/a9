5c6c6de855e8b784910dc78c8d0f2900c14888a5
Antjedotc <85960754+Antjedotc@users.noreply.github.com>
2023-04-23 13:30:23 -0400

    Update Quadtree.java

--- Quadtree.java
+++ Quadtree.java
@@ -10,15 +10,33 @@ public class Quadtree {
     private class Node {
 
         private Color color;
-        
+
+        private Node NE;
+        private Node SE;
+        private Node SW;
+        private Node NW;
+
+        //constructor using a color
         protected Node(Color color){
             this.color = color;
         }
 
+        //constructor using rgb values
         protected Node(int r, int g, int b){
             this.color = new Color(int r, int g, int b);
         }
 
+        //getters
+        protected Node getNE(){ return this.NE; }
+        protected Node getSE(){ return this.SE; }
+        protected Node getSW(){ return this.SW; }
+        protected Node getNW(){ return this.NW; }
+
+        //setters
+        protected void setNE(Node NE){ this.NE = NE; }
+        protected void setSE(Node SE){ this.SE = SE; }
+        protected void setSW(Node SW){ this.SW = SW; }
+        protected void setNE(Node NW){ this.NW = NW; }
 
     }
     //-----------END OF PRIVATE NODE CLASS
