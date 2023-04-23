3f1afedaf7a383bffb68f9ff5f948df0a5a35ea4
Antjedotc <85960754+Antjedotc@users.noreply.github.com>
2023-04-23 13:15:12 -0400

    Create Quadtree.java

---
+++ Quadtree.java
@@ -0,0 +1,16 @@
+public class Quadtree {
+
+    protected Node root;
+
+    public Quadtree () {
+
+    }
+
+    //-----------START OF PRIVATE NODE CLASS
+    private class Node {
+
+        private Color color;
+
+    }
+    //-----------END OF PRIVATE NODE CLASS
+}
